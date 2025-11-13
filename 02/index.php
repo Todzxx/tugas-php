baf0032a122f3d6d7390c407cf228d2b382c928f
yayan <githabaja@gmail.com>
2025-11-13 22:56:51 +0700

    Memperbarui file index.php di folder 02 dengan contoh sintaks

--- 02/index.php
+++ 02/index.php
@@ -1,8 +1,29 @@
 <?php
-// File ini akan berisi penjelasan tentang sintaks dasar PHP.
+// File ini berisi penjelasan tentang sintaks dasar PHP.
 
 // Tag PHP
 // Kode PHP diawali dengan <?php dan diakhiri dengan ?>
+
+// Komentar di PHP
+// Ini adalah komentar satu baris
+# Ini juga komentar satu baris
+/*
+Ini adalah blok komentar
+yang bisa terdiri dari
+beberapa baris.
+*/
+
+// PHP Case Sensitivity
+// Perintah, fungsi, dan class bawaan PHP tidak case-sensitive.
+echo "Hello World!<br>";
+ECHO "Hello World!<br>";
+EcHo "Hello World!<br>";
+
+// Namun, semua nama variabel adalah case-sensitive.
+$warna = "merah";
+echo "Mobilku berwarna " . $warna . "<br>";
+// Baris berikut akan menghasilkan error karena variabel $WARNA tidak didefinisikan.
+// echo "Kapalku berwarna " . $WARNA . "<br>";
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,7 +32,8 @@
 <h1>Halaman PHP Pertamaku</h1>
 
 <?php
-echo "Hello World!";
+// Menampilkan output ke browser
+echo "Belajar Sintaks Dasar PHP!";
 ?>
 
 </body>
