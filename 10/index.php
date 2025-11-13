53faf92272a83f099a11c5b2a1fab32a0ddfe870
yayan <githabaja@gmail.com>
2025-11-14 00:11:04 +0700

    Menambahkan penjelasan parameter fungsi pada index.php di folder 10

--- 10/index.php
+++ 10/index.php
@@ -22,4 +22,26 @@ function tampilkanPesan() {
     echo "Ini adalah pesan dari fungsi.<br>";
 }
 tampilkanPesan();
+echo "<hr>";
+
+// 2. Parameter Fungsi
+echo "<h2>2. Parameter Fungsi</h2>";
+// Parameter adalah variabel yang digunakan untuk menerima input saat fungsi dipanggil.
+
+// Fungsi dengan satu parameter
+function sapaNama($nama) {
+    echo "Halo, " . $nama . "! Selamat datang.<br>";
+}
+
+sapaNama("Budi");
+sapaNama("Ani");
+
+// Fungsi dengan beberapa parameter
+function hitungJumlah($angka1, $angka2) {
+    $jumlah = $angka1 + $angka2;
+    echo "Jumlah dari " . $angka1 . " + " . $angka2 . " adalah " . $jumlah . "<br>";
+}
+
+hitungJumlah(10, 5);
+hitungJumlah(20, 15);
 ?>
