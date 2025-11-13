e442598d330bde9322ece29b9ace0af83c7e7176
yayan <githabaja@gmail.com>
2025-11-13 22:29:01 +0700

    Memperbarui index.php dengan contoh variabel dan operator

--- 01/index.php
+++ 01/index.php
@@ -1,3 +1,30 @@
 <?php
-echo "Hello, World!";
+// Ini adalah komentar satu baris
+
+/*
+Ini adalah
+komentar multi-baris
+*/
+
+// Variabel untuk menyimpan data
+$salam = "Halo, Dunia!";
+$angka1 = 10;
+$angka2 = 5;
+
+// Menampilkan output
+echo $salam;
+echo "<br>"; // Tag HTML untuk baris baru
+
+// Operator Aritmatika
+echo "Penjumlahan: " . ($angka1 + $angka2) . "<br>";
+echo "Pengurangan: " . ($angka1 - $angka2) . "<br>";
+echo "Perkalian: " . ($angka1 * $angka2) . "<br>";
+echo "Pembagian: " . ($angka1 / $angka2) . "<br>";
+
+// Struktur Kontrol: Percabangan
+if ($angka1 > $angka2) {
+    echo "Angka 1 lebih besar dari Angka 2";
+} else {
+    echo "Angka 1 tidak lebih besar dari Angka 2";
+}
 ?>
