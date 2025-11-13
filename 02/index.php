d01819d170047193dd6a6fa3c602ca32f048f56f
yayan <githabaja@gmail.com>
2025-11-13 22:55:45 +0700

    Menambahkan file index.php di folder 02 dengan sintaks dasar

---
+++ 02/index.php
@@ -0,0 +1,18 @@
+<?php
+// File ini akan berisi penjelasan tentang sintaks dasar PHP.
+
+// Tag PHP
+// Kode PHP diawali dengan <?php dan diakhiri dengan ?>
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Halaman PHP Pertamaku</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
